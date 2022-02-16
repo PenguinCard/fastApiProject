a4e66f4e156d502d73a3955e60f7fa45b2c1bf88
김진수 <kimjinsu@gimjinsuui-MacBookAir.local>
2022-02-17 01:07:52 +0900

    엑셀 병합 기능 추가

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
+
+### 라이브러리 설치
+
+<br>
+
+#### fastapi 설치
     pip install fastapi
 <br>
 
+#### python-multipart 설치 (fast api 파일 수신 부)
     pip install python-multipart
 <br>
     
+#### python 서버    
     pip install uvicorn
+<br>
+
+#### openpyxl 설치(엑셀파일 읽고 쓰기)
+    pip install openpyxl
+
+<br>
 <br>
     
 ### 서버 실행
--- routes/files.py
+++ routes/files.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Response
 
+from openpyxl import load_workbook, Workbook
+
+from io import BytesIO
 from typing import List
 
 router = APIRouter(prefix="/api/file", tags=["file"])
@@ -8,10 +11,29 @@ router = APIRouter(prefix="/api/file", tags=["file"])
 async def img_root():
     return "file root"
 
+# file 작업 multifile 파일 수신
 @router.post("/work")
-async def file_work(file: List[UploadFile] = File(...)):
-    print(file)
-    # for f in file:
-    #     content = await f.read()
-    #     print(str(content))
-    return "file work"
+async def file_work(files: List[UploadFile] = File(...)):
+    
+    write_wb = Workbook()
+    write_ws = write_wb.active
+    
+    xlsx_files = list(filter(lambda file: True if file.filename.find('.xlsx') >= 0 else False, files))
+    
+    for xlsx_file in xlsx_files:
+        wb = load_workbook(filename=BytesIO(xlsx_file.file.read()))
+        ws = wb.active
+        
+        idx = 1;
+        
+        for row in ws.iter_rows(values_only=True):
+            if idx != 1:
+                write_ws.append(row)
+            idx = idx + 1
+            
+    io = BytesIO()
+    write_wb.save(io)
+    
+    headers = { 'Content-Disposition': 'attachment; filename="union.xlsx"' }
+    
+    return Response(io.getvalue(), headers=headers)
